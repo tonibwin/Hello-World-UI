ec4f38784a163e9aacc89b7630dd1f40e20e05d2
Destoni Baldwin <tonibwin@hotmail.com>
2018-10-23 19:24:44 -0600

    second commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Route exact path={'/'} render={ (routerProps) => < Home routerProps={routerProps} setUpGame={this.setUpGame} />} />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import './index.css';
 
 //import App from './App';
@@ -16,24 +17,26 @@ import './index.css';
 class App extends Component {
     render() {
         return (
-            <body className="App" style={{
-                position: 'absolute', 
-                top:'0', 
-                bottom:'0', 
-                left:'0', 
-                right:'0',
-                width: '100% !important', 
-                height: '100% !important',
-                backgroundPosition: 'center',
-                backgroundSize: 'cover',
-                backgroundRepeat: 'noRepeat',
-                WebkitTransition: 'all', // note the capital 'W' here
-                msTransition: 'all',
-                backgroundImage: "url(" + "https://images.unsplash.com/photo-1475965894430-b05c9d13568a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a642a3711c10d64eb161e1ef9bc4fd1e&auto=format&fit=crop&w=975&q=80" + ")"}}>
-                <div className="box-shadow">
-                    <h1 className="font-families">H E L L O  W O R L D</h1>
-                </div>
-            </body>
+            <HashRouter>
+                <body className="App" style={{
+                    position: 'absolute', 
+                    top:'0', 
+                    bottom:'0', 
+                    left:'0', 
+                    right:'0',
+                    width: '100% !important', 
+                    height: '100% !important',
+                    backgroundPosition: 'center',
+                    backgroundSize: 'cover',
+                    backgroundRepeat: 'noRepeat',
+                    WebkitTransition: 'all', // note the capital 'W' here
+                    msTransition: 'all',
+                    backgroundImage: "url(" + "https://images.unsplash.com/photo-1475965894430-b05c9d13568a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a642a3711c10d64eb161e1ef9bc4fd1e&auto=format&fit=crop&w=975&q=80" + ")"}}>
+                    <div className="box-shadow">
+                        <h1 className="font-families">H E L L O  W O R L D</h1>
+                    </div>
+                </body>
+            </HashRouter>
         );
     }
 }
